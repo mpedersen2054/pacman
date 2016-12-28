e2e4f4e0cff8d5b083fe1f48001d8f60f5fdca42
Matt Pedersen <matthewnpedersen@gmail.com>
2016-12-27 21:06:31 -0800

    make the pacman background image rotate(change direction) based on his current direction

--- main.js
+++ main.js
@@ -30,9 +30,9 @@ var world = [
   [2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2],
   [2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2],
   [2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2],
-  [2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2],
-  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-  [2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2],
+  [2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 0, 0, 0, 0, 0, 0, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 0, 0, 0, 0, 0, 0, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 0, 0, 0, 0, 0, 0, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2],
   [2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2],
   [2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2],
   [2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 1, 2, 2, 2, 2, 2, 2],
@@ -78,7 +78,7 @@ function displayWorld() {
 }
 
 function displayPacman() {
-  console.log(pacman)
+  // console.log(pacman)
   $('#pacman').css({'top': `${pacman.y * 20}px`})
   $('#pacman').css({'left': `${pacman.x * 20}px`})
 }
@@ -87,41 +87,89 @@ function displayScore() {
   $('.game-score').html(pacman.score)
 }
 
+// only used to show the time, the gameTick is
+// every 3milli, we want this to show every sec
 var time = 0
 function showTime() {
   time++
   $('#time').html(time)
 }
 
+// updates the game every 3 milliseconds
 function gameTick() {
-  // console.log(pacman.direction)
   movePacman()
 }
 
+$('#pacman').css({
+  '-moz-transform': 'rotate(360deg)',
+  '-webkit-transform': 'rotate(360deg)'
+})
+
+// original rot 0
+// left rot 180deg
+// up rot 270deg
+// right rot original
+// down rot 90deg
+
+function updatePacmanDirection() {
+  var $pacman = $('#pacman')
+  if (pacman.direction == -1) {
+    $pacman.css({
+      '-moz-transform': 'rotate(180deg)',
+      '-webkit-transform': 'rotate(180deg)'
+    })
+  }
+  if (pacman.direction == -2) {
+    $pacman.css({
+      '-moz-transform': 'rotate(270deg)',
+      '-webkit-transform': 'rotate(270deg)'
+    })
+  }
+  if (pacman.direction == 1) {
+    $pacman.css({
+      '-moz-transform': 'rotate(360deg)',
+      '-webkit-transform': 'rotate(360deg)'
+    })
+  }
+  if (pacman.direction == 2) {
+    $pacman.css({
+      '-moz-transform': 'rotate(90deg)',
+      '-webkit-transform': 'rotate(90deg)'
+    })
+  }
+}
+
 function movePacman(keyCode) {
-  if (keyCode == 37 || pacman.direction == -1 && world[pacman.y][pacman.x-1] !== 2) { // left
-      pacman.x--
-    }
-    else if (keyCode == 38 || pacman.direction == -2 && world[pacman.y-1][pacman.x] !== 2) { // up
-      pacman.y--
-    }
-    else if (keyCode == 39 || pacman.direction == 1 && world[pacman.y][pacman.x+1] !== 2) { // right
-      pacman.x++
-    }
-    else if (keyCode == 40 || pacman.direction == 2 && world[pacman.y+1][pacman.x] !== 2) { // down
-      pacman.y++
-    }
 
-    // y is y-axis, moves up and down arrs in the arr of arrs
-    // x is x-axis, moves up and down array (0...X)
-    if (world[pacman.y][pacman.x] == 1) {
-      world[pacman.y][pacman.x] = 0
-      pacman.score += 10
-      displayScore()
-      displayWorld()
-    }
+  // check if pacman is running into a well, if not move him in the direction hes going
 
-    displayPacman()
+  if (pacman.direction == -1 && world[pacman.y][pacman.x-1] !== 2) { // left
+    updatePacmanDirection()
+    pacman.x--
+  }
+  else if (pacman.direction == -2 && world[pacman.y-1][pacman.x] !== 2) { // up
+    updatePacmanDirection()
+    pacman.y--
+  }
+  else if (pacman.direction == 1 && world[pacman.y][pacman.x+1] !== 2) { // right
+    updatePacmanDirection()
+    pacman.x++
+  }
+  else if (pacman.direction == 2 && world[pacman.y+1][pacman.x] !== 2) { // down
+    updatePacmanDirection()
+    pacman.y++
+  }
+
+  // y is y-axis, moves up and down arrs in the arr of arrs
+  // x is x-axis, moves up and down array (0...X)
+  if (world[pacman.y][pacman.x] == 1) {
+    world[pacman.y][pacman.x] = 0
+    pacman.score += 10
+    displayScore()
+    displayWorld()
+  }
+
+  displayPacman()
 }
 
 
@@ -134,14 +182,10 @@ var _time = setInterval(showTime, 1000)
 
 
 $(document).keydown(function(e) {
-  // left: 37, up: 38, right: 39, down: 40
-  // console.log(`pressed key ${e.keyCode}`)
-
+  // update pacmans current direction based on key pressed
   if (e.keyCode == 37) pacman.direction = -1
   if (e.keyCode == 38) pacman.direction = -2
   if (e.keyCode == 39) pacman.direction = 1
   if (e.keyCode == 40) pacman.direction = 2
-
-  // movePacman(e.keyCode)
 
 })
