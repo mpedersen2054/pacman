122b2e76c4dfb9f0c05384901be72a2c2cb08d8e
Matt Pedersen <matthewnpedersen@gmail.com>
2016-12-27 17:31:18 -0800

    fix workaround to remove whitespaces from inline-block, begin to write keydown event to move pacmane

--- main.js
+++ main.js
@@ -40,10 +40,12 @@ var world2 = [
 function displayWorld(world) {
   var output = ''
 
-  for (var i = 0; i < world.length; i++) { // each arr inside world arr
+  // each arr inside world arr
+  for (var i = 0; i < world.length; i++) {
     output += '\n<div class="row">' // puts everything inside a row
 
-    for (var j = 0; j < world[i].length; j++) { // each indv rows vals
+    // each indv rows vals
+    for (var j = 0; j < world[i].length; j++) {
       if (world[i][j] == 2) {
         output += '\n\t<div class="brick"></div>'
       } else if (world[i][j] == 1) {
@@ -56,12 +58,17 @@ function displayWorld(world) {
     output += '\n</div>' // close div.row
   }
 
-  // console.log(output)
   $('#world').append(output)
 }
 
 
-
 $(function() {
+
   displayWorld(world2)
+
+  $(document).on('keydown', function(e) {
+    // left: 37, up: 38, right: 39, down: 40
+    console.log(`pressed key ${e.keyCode}`)
+  })
+
 })
